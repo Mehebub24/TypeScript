7489bacbad8c4357df54cbac302ce5987c6f140b
Mehebub24 <Mehebubkalapati@gmail.com>
2024-02-05 00:27:17 +0530

    function

---
+++ 2_function/function.js
@@ -0,0 +1,15 @@
+function addTwo(num) {
+    //num.toUpperCase() this is happens when ANY type
+    return 2 + num;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, elmail, isPaid) {
+}
+var loginUser = function (name, elmail, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("Mehebub", "Mehebub@gmail.com");
+getUpper("mehebub");
+addTwo(4);
---
+++ 2_function/function.ts
@@ -0,0 +1,35 @@
+function addTwo(num:number):number{
+    //num.toUpperCase() this is happens when ANY type
+    return 2 + num
+    //return "Mehebub"
+}
+
+function getUpper(val:string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name:string,elmail:string,isPaid:boolean){
+
+}
+let loginUser = (name:string, elmail: string,isPaid = false) => {
+
+}
+
+loginUser("Mehebub","Mehebub@gmail.com")
+getUpper("mehebub")
+addTwo(4)
+
+
+
+// function getValue(myNumbe:number){
+//     if(myNumbe > 50){
+//         return true
+//     }
+//     return"600 is OK"
+// }
+const getHello = (s:string):string => {
+    return "Hello"
+}
+
+
+export{}
