9cb5b9f992dc110f8c0b08a5cbca3e3eae0ec64f
Mehebub24 <Mehebubkalapati@gmail.com>
2024-02-07 02:02:03 +0530

    type Aliases

---
+++ 1_lets_learn/typeAliases.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function creatUser(user) {
+}
+creatUser({ name: "", email: "", isActive: true });
---
+++ 1_lets_learn/typeAliases.ts
@@ -0,0 +1,13 @@
+type User = {
+    name: string;
+    email: string ;
+    isActive: boolean
+}
+
+function creatUser(user: User){
+
+}
+
+creatUser({name:"",email:"",isActive:true})
+
+export{}
