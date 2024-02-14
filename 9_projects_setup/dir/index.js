1e14c9b927032907c0402e930568c9b84c62e445
Mehebub24 <Mehebubkalapati@gmail.com>
2024-02-14 13:06:22 +0530

    project setup

---
+++ 9_projects_setup/dir/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hi");
+console.log("hi");
+console.log("Hello");
+console.log("hello");
+console.log("hi");
---
+++ 9_projects_setup/src/index.ts
@@ -0,0 +1,8 @@
+console.log("hi")
+
+console.log("hi");
+
+console.log("Hello");
+console.log("hello");
+console.log("hi");
+
